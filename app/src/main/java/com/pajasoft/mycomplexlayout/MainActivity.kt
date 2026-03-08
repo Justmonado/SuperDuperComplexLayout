a15f147a3755080f058475b22b82f6fe4d2033b7
Alfonso Soots <alfonso_soots@hotmail.com>
2026-03-07 20:24:43 -0600

    SuperDuperComplexLayout

---
+++ app/src/main/java/com/pajasoft/mycomplexlayout/MainActivity.kt
@@ -0,0 +1,170 @@
+package com.pajasoft.mycomplexlayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.pajasoft.mycomplexlayout.ui.theme.MyComplexLayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyComplexLayoutTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    myComplexLayout(innerPadding)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun myComplexLayout(innerPadding: PaddingValues) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .padding(top = 25.dp, bottom = 25.dp)
+    ) {
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp)
+                .background(Color.Cyan),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Encabezado", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color.Yellow),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text("Caja 1")
+                Button(onClick = {}) {
+                    Text("Acción")
+                }
+            }
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color.Green),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text("Caja 2")
+                Button(onClick = {}) {
+                    Text("Acción")
+                }
+            }
+        }
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .background(Color.LightGray)
+        ) {
+
+            Text(
+            "Lista de Elementos",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(10.dp)
+        )
+            LazyColumn(
+                modifier = Modifier
+                    .background(Color.LightGray)
+                    .weight(1f)
+                    .padding(horizontal = 10.dp)
+
+            ) {
+
+                val comidas = listOf("Hamburguesa", "Pizza", "Tacos", "Sushi","Pescado","Camarones","Kebabs","Torta")
+
+                items(comidas) { comida ->
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 6.dp)
+                            .background(Color.Gray, RoundedCornerShape(20.dp))
+                            .padding(15.dp)
+                    ) {
+                        Text(comida)
+                    }
+                }
+            }
+
+
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp)
+                .background(Color.Magenta),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Pie de página", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun GreetingPreview() {
+    MyComplexLayoutTheme {
+        myComplexLayout(innerPadding = PaddingValues(10.dp))
+    }
+}
